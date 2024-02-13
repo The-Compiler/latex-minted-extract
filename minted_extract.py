bdeebed9aba1ca0ec25f54fab65bd188d4314a26
Florian Bruhin <me@the-compiler.org>
2024-02-13 21:54:54 +0100

    Get highlighting to run

--- minted_extract.py
+++ minted_extract.py
@@ -6,10 +6,15 @@ import fnmatch
 PATTERN_SEP = "!"
 
 
+class Error(Exception):
+    pass
+
+
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     parser.add_argument("--minted-lang", help="Language for minted")
     parser.add_argument("--minted-opts", help="Options for minted")
+    parser.add_argument("--highlight", help="Patterns to highlight")
     parser.add_argument("file", help="Source file to read")
     parser.add_argument(
         "patterns", help=f"Patterns to match, separated by '{PATTERN_SEP}'"
@@ -17,33 +22,52 @@ def parse_args() -> argparse.Namespace:
     return parser.parse_args()
 
 
+def match_patterns(lines: list[str], patterns: list[str]) -> tuple[int, int]:
+    num_lines = list(enumerate(lines, start=1))
+    n = None
+    start = None
+
+    for pattern in patterns:
+        while num_lines:
+            n, line = num_lines.pop(0)
+
+            if fnmatch.fnmatchcase(line.strip(), pattern) or (
+                not pattern and not line.strip()
+            ):
+                if start is None:
+                    start = n
+                break
+        else:
+            raise Error(f"Remains unmatched: {pattern}")
+
+    assert start is not None
+    assert n is not None
+    return start, n
+
+
 def main() -> None:
     args = parse_args()
+    #print(r"\begin{minted}{python}")
+    #for a in sys.argv:
+    #    print(repr(a))
+    #print(r"\end{minted}")
 
     with open(args.file) as f:
         lines = f.readlines()
 
     patterns = args.patterns.split(PATTERN_SEP)
-    output = []
-    started = False
+    highlight_patterns = args.highlight.split(PATTERN_SEP)
+    minted_opts = args.minted_opts.split(",")
 
-    for pattern in patterns:
-        while lines:
-            line = lines.pop(0)
-            if started:
-                output.append(line)
+    start, end = match_patterns(lines, patterns)
+    output = lines[start - 1 : end]
 
-            if fnmatch.fnmatchcase(line.strip(), pattern):
-                if not started:
-                    output.append(line)
-                started = True
-                break
-        else:
-            msg = f"Remains unmatched: {pattern}"
-            print(r"\errmessage{%s}" % msg)
-            sys.exit(msg)
+    if highlight_patterns:
+        # FIXME multiple
+        hl_start, hl_end = match_patterns(output, highlight_patterns)
+        minted_opts.append("highlightlines={%s-%s}" % (hl_start, hl_end))
 
-    opts = f"[{args.minted_opts}]" if args.minted_opts else ""
+    opts = f"[{','.join(minted_opts)}]" if minted_opts else ""
     print(r"\begin{minted}%s{%s}" % (opts, args.minted_lang))
     print("".join(output))
     print(r"\end{minted}")
