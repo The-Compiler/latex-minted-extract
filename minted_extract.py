3175a331d2cb60ef07e52aeec5cf4ebb6820a2b4
Florian Bruhin <me@the-compiler.org>
2024-02-13 21:02:09 +0100

    Add minted opts and language

--- minted_extract.py
+++ minted_extract.py
@@ -1,11 +1,19 @@
 # based on https://tex.stackexchange.com/a/130755
+import sys
 import argparse
 import fnmatch
 
+PATTERN_SEP = "!"
+
+
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
-    parser.add_argument("file")
-    parser.add_argument("patterns")
+    parser.add_argument("--minted-lang", help="Language for minted")
+    parser.add_argument("--minted-opts", help="Options for minted")
+    parser.add_argument("file", help="Source file to read")
+    parser.add_argument(
+        "patterns", help=f"Patterns to match, separated by '{PATTERN_SEP}'"
+    )
     return parser.parse_args()
 
 
@@ -15,27 +23,29 @@ def main() -> None:
     with open(args.file) as f:
         lines = f.readlines()
 
-    patterns = args.patterns.split(",")
-    pattern = None
+    patterns = args.patterns.split(PATTERN_SEP)
+    output = []
     started = False
-
-    print(r"\begin{minted}{python}")
 
     for pattern in patterns:
         while lines:
             line = lines.pop(0)
             if started:
-                print(line, end="")
+                output.append(line)
 
             if fnmatch.fnmatchcase(line.strip(), pattern):
                 if not started:
-                    print(line, end="")
+                    output.append(line)
                 started = True
                 break
+        else:
+            msg = f"Remains unmatched: {pattern}"
+            print(r"\errmessage{%s}" % msg)
+            sys.exit(msg)
 
-    if not started:
-        print(f"Pattern {pattern!r} not found")
-
+    opts = f"[{args.minted_opts}]" if args.minted_opts else ""
+    print(r"\begin{minted}%s{%s}" % (opts, args.minted_lang))
+    print("".join(output))
     print(r"\end{minted}")
 
 
