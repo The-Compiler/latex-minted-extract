b10c0c65a2c379303fe468c1393049dafb94bcc3
Florian Bruhin <me@the-compiler.org>
2024-02-13 20:43:35 +0100

    Initial pattern matching

--- minted_extract.py
+++ minted_extract.py
@@ -1,23 +1,41 @@
 # based on https://tex.stackexchange.com/a/130755
 import argparse
-import sys
+import fnmatch
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
-    parser.add_argument("codefile")
-    parser.add_argument("delim1")
-    parser.add_argument("delim2")
+    parser.add_argument("file")
+    parser.add_argument("patterns")
     return parser.parse_args()
 
 
 def main() -> None:
     args = parse_args()
 
-    with open(args.codefile) as f:
-        code = f.read()
+    with open(args.file) as f:
+        lines = f.readlines()
+
+    patterns = args.patterns.split(",")
+    pattern = None
+    started = False
 
     print(r"\begin{minted}{python}")
-    print(code.split(args.delim1, 1)[1].split(args.delim2, 1)[0])
+
+    for pattern in patterns:
+        while lines:
+            line = lines.pop(0)
+            if started:
+                print(line, end="")
+
+            if fnmatch.fnmatchcase(line.strip(), pattern):
+                if not started:
+                    print(line, end="")
+                started = True
+                break
+
+    if not started:
+        print(f"Pattern {pattern!r} not found")
+
     print(r"\end{minted}")
 
 
