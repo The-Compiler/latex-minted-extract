cc33a32c42b2d26accace805fc8490f47e1a6346
Florian Bruhin <me@the-compiler.org>
2024-02-14 16:48:54 +0100

    Initial new approach

--- minted_extract.py
+++ minted_extract.py
@@ -1,10 +1,26 @@
-# based on https://tex.stackexchange.com/a/130755
-import sys
+# Originally inspired by https://tex.stackexchange.com/a/130755
+import collections
 import argparse
-import fnmatch
+import enum
+import re
+from typing import Iterator
 
-PATTERN_SEP = "!"
-HIGHLIGHT_SEP = "!!"
+
+COMMENT = "#@ "
+
+
+class Token(enum.Enum):
+    START = "<"
+    END = ">"
+    HL = "!"
+    HL_START = "!<"
+    HL_END = "!>"
+    CONTEXT = "..."
+
+
+# e.g.: def func():  #@ ! slide-1
+# ->                       def func():    #@        !                slide-1
+COMMENT_RE = re.compile(rf"(?P<code>.*)\s*{COMMENT}(?P<token>[^ ]*) (?P<snippet>.*)")
 
 
 class Error(Exception):
@@ -15,38 +31,41 @@ def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     parser.add_argument("--minted-lang", help="Language for minted")
     parser.add_argument("--minted-opts", help="Options for minted", default="")
-    parser.add_argument("--highlight", help="Patterns to highlight")
     parser.add_argument(  # somewhat awkard to make the LaTeX side easier
-        "--show-name", help="Also show file name", type=bool, choices=[0, 1]
+        "--show-name",
+        help="Also show file name",
+        type=lambda v: bool(int(v)),
+        choices=[0, 1],
     )
     parser.add_argument("file", help="Source file to read")
-    parser.add_argument(
-        "patterns", help=f"Patterns to match, separated by '{PATTERN_SEP}'"
-    )
+    parser.add_argument("snippet", help="Snippet name to extract")
     return parser.parse_args()
 
 
-def match_patterns(lines: list[str], patterns: list[str]) -> tuple[int, int]:
-    num_lines = list(enumerate(lines, start=1))
-    n = None
-    start = None
+def tokens_to_minted_opts(tokens: list[tuple[int, Token]]) -> Iterator[str]:
+    hl_start = None
+    hl_ranges = []
 
-    for pattern in patterns:
-        while num_lines:
-            n, line = num_lines.pop(0)
-
-            if fnmatch.fnmatchcase(line.strip(), pattern) or (
-                not pattern and not line.strip()
-            ):
-                if start is None:
-                    start = n
-                break
+    for lineno, token in tokens:
+        if token == Token.START:
+            yield f"firstline={lineno}"
+        elif token == Token.END:
+            yield f"lastline={lineno}"
+        elif token == Token.HL:
+            hl_ranges.append(str(lineno))
+        elif token == Token.HL_START:
+            hl_start = lineno
+        elif token == Token.HL_END:
+            assert hl_start is not None
+            hl_ranges.append(f"{hl_start}-{lineno}")
+            hl_start = None
+        elif token == Token.CONTEXT:
+            raise Error("Context not supported yet")
         else:
-            raise Error(f"Remains unmatched: {pattern}")
+            raise Error(f"Unknown token: {token}")
 
-    assert start is not None
-    assert n is not None
-    return start, n
+    if hl_ranges:
+        yield f"highlightlines={','.join(hl_ranges)}"
 
 
 def main() -> None:
@@ -59,25 +78,32 @@ def main() -> None:
     with open(args.file) as f:
         lines = f.read().splitlines()
 
+    clean_lines = []
     minted_opts = args.minted_opts.split(",")
 
-    start, end = match_patterns(lines, args.patterns.split(PATTERN_SEP))
-    output = lines[start - 1 : end]
+    # Mapping snippet name to list of (lineno, token)
+    tokens: dict[str, list[tuple[int, Token]]] = collections.defaultdict(list)
 
-    if args.highlight:
-        highlight_pairs = [
-            match_patterns(output, patterns.split(PATTERN_SEP))
-            for patterns in args.highlight.split(HIGHLIGHT_SEP)
-        ]
-        val = ",".join(f"{start}-{end}" for start, end in highlight_pairs)
-        minted_opts.append("highlightlines={%s}" % val)
+    for lineno, line in enumerate(lines, start=1):
+        if COMMENT not in line:
+            clean_lines.append(line)
+            continue
+
+        match = COMMENT_RE.match(line)
+        assert match is not None, match
+
+        clean_lines.append(match.group("code"))
+        token = Token(match.group("token"))
+        tokens[match.group("snippet")].append((lineno, token))
+
+    minted_opts += list(tokens_to_minted_opts(tokens[args.snippets]))
 
     if args.show_name:
         print(r"\mintinline{python}{# %s}" % args.file)
 
-    opts = f"[{','.join(minted_opts)}]" if minted_opts else ""
-    print(r"\begin{minted}%s{%s}" % (opts, args.minted_lang))
-    print("\n".join(output))
+    minted_opts_str = ','.join(minted_opts)
+    print(r"\begin{minted}[%s]{%s}" % (minted_opts_str, args.minted_lang))
+    print("\n".join(clean_lines))
     print(r"\end{minted}")
 
 
