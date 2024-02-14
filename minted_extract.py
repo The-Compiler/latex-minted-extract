408f627e55aa18de285a51ff81c71427ce519ab9
Florian Bruhin <me@the-compiler.org>
2024-02-14 15:33:19 +0100

    Add showing filename and multiple highlights

--- minted_extract.py
+++ minted_extract.py
@@ -4,6 +4,7 @@ import argparse
 import fnmatch
 
 PATTERN_SEP = "!"
+HIGHLIGHT_SEP = "!!"
 
 
 class Error(Exception):
@@ -15,6 +16,9 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument("--minted-lang", help="Language for minted")
     parser.add_argument("--minted-opts", help="Options for minted", default="")
     parser.add_argument("--highlight", help="Patterns to highlight")
+    parser.add_argument(  # somewhat awkard to make the LaTeX side easier
+        "--show-name", help="Also show file name", type=bool, choices=[0, 1]
+    )
     parser.add_argument("file", help="Source file to read")
     parser.add_argument(
         "patterns", help=f"Patterns to match, separated by '{PATTERN_SEP}'"
@@ -47,25 +51,31 @@ def match_patterns(lines: list[str], patterns: list[str]) -> tuple[int, int]:
 
 def main() -> None:
     args = parse_args()
-    #print(r"\begin{minted}{python}")
-    #for a in sys.argv:
+    # print(r"\begin{minted}{python}")
+    # for a in sys.argv:
     #    print(repr(a))
-    #print(r"\end{minted}")
+    # print(r"\end{minted}")
 
     with open(args.file) as f:
         lines = f.read().splitlines()
 
     patterns = args.patterns.split(PATTERN_SEP)
-    highlight_patterns = args.highlight.split(PATTERN_SEP)
+    highlight_patterns = args.highlight.split(HIGHLIGHT_SEP)
     minted_opts = args.minted_opts.split(",")
 
     start, end = match_patterns(lines, patterns)
     output = lines[start - 1 : end]
 
-    if highlight_patterns:
-        # FIXME multiple
-        hl_start, hl_end = match_patterns(output, highlight_patterns)
-        minted_opts.append("highlightlines={%s-%s}" % (hl_start, hl_end))
+    highlight_pairs = [
+        match_patterns(output, patterns.split(PATTERN_SEP))
+        for patterns in highlight_patterns
+    ]
+    if highlight_pairs:
+        val = ",".join(f"{start}-{end}" for start, end in highlight_pairs)
+        minted_opts.append("highlightlines={%s}" % val)
+
+    if args.show_name:
+        print(r"\mintinline{python}{# %s}" % args.file)
 
     opts = f"[{','.join(minted_opts)}]" if minted_opts else ""
     print(r"\begin{minted}%s{%s}" % (opts, args.minted_lang))
