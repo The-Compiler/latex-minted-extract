1ed2424aaac7ff7f359b69b6eda9dd8f283f3a7d
Florian Bruhin <me@the-compiler.org>
2025-03-27 15:55:51 +0100

    Update from training repo

--- minted_extract.py
+++ minted_extract.py
@@ -5,13 +5,15 @@
 # Originally inspired by https://tex.stackexchange.com/a/130755
 from __future__ import annotations
 
-import re
+import re  # <- imports-[12]
+import shlex
+import dataclasses
 import collections
 import argparse
-import pathlib
-import enum
+import pathlib  # !! imports-1
+import enum # !! imports-2
 import sys
-from typing import Iterator
+from typing import Iterator  # -> imports-[12]
 
 
 class Token(enum.Enum):
@@ -20,48 +22,116 @@ class Token(enum.Enum):
     HL = "!!"
     HL_START = "<!"
     HL_END = "!>"
+    HL_INLINE = "||"
+    INLINE_REPLACE = "s/"
+    JINJA = "%%"
+
+
+_TokenList = list[tuple[int, Token, list[str]]]
 
 
 TOKEN_PAT = r"|".join(re.escape(token.value) for token in Token)
-COMMENT_RE = re.compile(rf"(?P<code>.*) +# (?P<comment>({TOKEN_PAT}) .*)")
+COMMENT_RE = re.compile(rf"(?P<code>.*) *# (?P<comment>({TOKEN_PAT}) .*)")
+EX_MARKER_RE = re.compile(r"\s*# exercise: \[(?P<label>.*)\]")
 
 
 class Error(Exception):
     pass
 
 
+class OutputType(enum.Enum):
+    #: The distributed code.zip
+    CODE = "code"
+    #: The solution zip
+    SOLUTION = "solution"
+    #: Shown on the slides
+    SLIDE = "slide"
+    #: Where pytest runs without errors
+    SELFTEST = "selftests"
+
+
+@dataclasses.dataclass
+class ParsedLine:
+    lineno: int  # only for debugging
+    code: str  # for non-TokenLine, the whole line
+
+
+@dataclasses.dataclass
+class TokenLine(ParsedLine):
+    comment: str
+
+
+@dataclasses.dataclass
+class ExerciseMarkerLine(ParsedLine):
+    label: str
+
+    def to_solution(self) -> str:
+        return self.code.replace("exercise:", "solution:")
+
+
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     parser.add_argument("--minted-lang", help="Language for minted", default="python")
     parser.add_argument("--minted-opts", help="Options for minted", default="")
-    parser.add_argument(  # somewhat awkard to make the LaTeX side easier
+    parser.add_argument(
+        "--prefix",
+        help="Directory prefix for code files",
+        type=pathlib.Path,
+        default=pathlib.Path(),
+    )
+    parser.add_argument(
         "--show-name",
         help="Also show file name",
-        type=lambda v: bool(int(v)),
-        choices=[0, 1],
+        choices=["false", "true", "phantom"],
+    )
+    parser.add_argument(
+        "--output-type",
+        help="Which version of the code to output",
+        choices=[t.value for t in OutputType],
+        default=OutputType.SLIDE.value,
     )
     parser.add_argument("file", help="Source file to read", type=pathlib.Path)
     parser.add_argument("snippet", help="Snippet name to extract")
     return parser.parse_args()
 
 
-def tokens_to_minted_opts(
-    tokens: list[tuple[int, Token]], snippet: str
-) -> Iterator[str]:
+def shlex_split(s: str) -> list[str]:
+    """Reimplementation of shlex.split without backslash escaping."""
+    assert s is not None
+    lexer = shlex.shlex(s, posix=True)  # default for shlex.split
+    lexer.whitespace_split = True
+    lexer.escape = ""
+    return list(lexer)
+
+
+def format_token_list(tokens: _TokenList) -> str:
+    return "\n".join(
+        f"{lineno}: {token.value} {', '.join(token_args)}"
+        for lineno, token, token_args in tokens
+    )
+
+
+def tokens_to_minted_opts(tokens: _TokenList, snippet: str) -> Iterator[str]:
     hl_start = None
     hl_ranges = []
     has_start = False
     has_end = False
 
-    for lineno, token in tokens:
+    for lineno, token, token_args in tokens:
         if token == Token.START:
             if has_start:
-                raise Error(f"Multiple start tokens in {tokens} for {snippet}")
+                raise Error(
+                    f"Multiple start tokens for {snippet} in:\n"
+                    f"{format_token_list(tokens)}"
+                )
             has_start = True
             yield f"firstline={lineno}"
         elif token == Token.END:
             if has_end:
-                raise Error(f"Multiple end tokens in {tokens} for {snippet}")
+                raise Error(
+                    f"Multiple end tokens for {snippet} in:\n"
+                    f"{format_token_list(tokens)}"
+                )
             has_end = True
             yield f"lastline={lineno}"
             if hl_start is not None:  # ending snippet ends highlight
@@ -78,15 +148,25 @@ def tokens_to_minted_opts(
                 raise Error(f"Unmatched highlight end at line {lineno}")
             hl_ranges.append(f"{hl_start}-{lineno}")
             hl_start = None
+        elif token in [Token.HL_INLINE, Token.INLINE_REPLACE]:
+            yield "escapeinside=||"
         else:
-            raise Error(f"Unknown token: {token}")
+            raise Error(f"Unknown token for minted opts: {token}")
 
     if hl_ranges:
         value = ",".join(hl_ranges)
         yield "highlightlines={%s}" % value
 
     if not has_start or not has_end:
-        raise Error(f"Missing start/end tokens in {tokens} for {snippet}")
+        missing = []
+        if not has_start:
+            missing.append("start")
+        if not has_end:
+            missing.append("end")
+        raise Error(
+            f"Missing {'/'.join(missing)} token for {snippet} in:\n"
+            f"{format_token_list(tokens)}"
+        )
 
 
 def expand_snippet_name(snippet: str) -> Iterator[str]:
@@ -103,58 +183,186 @@ def expand_snippet_name(snippet: str) -> Iterator[str]:
         yield prefix + part + suffix
 
 
-def split_line(line: str) -> tuple[str, str | None]:
-    m = COMMENT_RE.match(line)
-    if m is None:
-        return line, None
+def parse_line(line: str, lineno: int) -> ParsedLine:
+    if (match := COMMENT_RE.match(line)) is not None:
+        code = match.group("code").rstrip(" ")
+        comment = match.group("comment")
+        return TokenLine(code=code, comment=comment, lineno=lineno)
+    elif (match := EX_MARKER_RE.match(line)) is not None:
+        label = match.group("label")
+        return ExerciseMarkerLine(code=line, lineno=lineno, label=label)
+    else:
+        # Normal code line without special comment
+        return ParsedLine(code=line, lineno=lineno)
 
-    return m.group("code"), m.group("comment")
+
+def search_replace_code(code: str, search: str, replace: str, lineno: int) -> str:
+    not_found_msg = f"Failed to find {search!r} in {code!r} (line {lineno})"
+
+    if search.startswith("/") and search.endswith("/"):
+        try:
+            pattern = re.compile(search[1:-1])
+            if not pattern.search(code):
+                raise Error(not_found_msg)
+            return pattern.sub(replace.replace("\\", "\\\\"), code)
+        except re.error as e:
+            raise Error(f"{search!r}: {e}")
+
+    if search not in code:
+        raise Error(not_found_msg)
+    return code.replace(search, replace)
+
+
+def transform_code(lines: list[str], tokens: _TokenList) -> Iterator[str]:
+    code_tokens = collections.defaultdict(list)
+    for lineno, token, token_args in tokens:
+        if token in [Token.HL_INLINE, Token.INLINE_REPLACE]:
+            code_tokens[lineno].append((token, token_args))
+
+    for lineno, line in enumerate(lines, start=1):
+        parsed = parse_line(line, lineno)
+        code = parsed.code
+        for token, token_args in code_tokens[lineno]:
+            if token is Token.HL_INLINE:
+                [search] = token_args
+                replace = r"|\highlightbox{%s}|" % totex(search)
+                code = search_replace_code(code, search, replace, lineno)
+            elif token is Token.INLINE_REPLACE:
+                search, replace = token_args
+                code = search_replace_code(code, search, replace, lineno)
+            else:
+                assert False, token
+
+        yield code
+
+
+def totex(value: str) -> str:
+    # https://stackoverflow.com/q/16259923
+    replacements = {
+        "&": r"\&",
+        "%": r"\%",
+        "$": r"\$",
+        "#": r"\#",
+        "_": r"\_",
+        "{": r"\{",
+        "}": r"\}",
+        "~": r"\textasciitilde{}",
+        "^": r"\^{}",
+        "\\": r"\textbackslash{}",
+        "\n": "\\\\",
+    }
+    return "".join(replacements.get(c, c) for c in value)
+
+
+def use_jinja(path: pathlib.Path) -> bool:
+    return path.suffix in [".py", ".ini"]  # FIXME what about others?
+
+
+def jinja_prepare(code: str) -> str | None:
+    """Prepare the code for jinja if jinja comments are used."""
+    jinja_comment = f"# {Token.JINJA.value} "
+    output = []
+    uses_jinja = False
+    for lineno, line in enumerate(code.splitlines(), start=1):
+        has_jinja_syntax = any(marker in line for marker in ["{{", "{%"])
+        if line.strip().startswith(jinja_comment):
+            uses_jinja = True
+            output.append(line.replace(jinja_comment, ""))
+        elif line.strip().startswith("#") and has_jinja_syntax:
+            raise Error(f"Stray jinja syntax on line {lineno}: {line}")
+        else:
+            output.append(line)
+
+    return "\n".join(output) if uses_jinja else None
+
+
+def jinja_render(contents: str, output_type: OutputType) -> str:
+    import jinja2
+
+    return jinja2.Template(contents, trim_blocks=True, lstrip_blocks=True).render(
+        # FIXME should SELFTEST always imply SOLUTION too?
+        solution=output_type in [OutputType.SOLUTION, OutputType.SELFTEST],
+        selftest=output_type == OutputType.SELFTEST,
+        slide=output_type == OutputType.SLIDE,
+    )
+
+
+def read_code(file: pathlib.Path, output_type: OutputType) -> str:
+    code = file.read_text()
+    # Jinja needs to happen before going through the lines, as line numbers could shift.
+    # To avoid issues with leftovers "# %%" with trim_blocks=True, we cut those
+    # out first, before anything gets to Jinja. We also avoid using jinja if
+    # there are no "# %%" comments in the file, because importing jinja has a
+    # non-negligible time cost.
+    if use_jinja(file):
+        jinja_code = jinja_prepare(code)
+        if jinja_code is not None:
+            return jinja_render(jinja_code, output_type=output_type)
+    return code
+
+
+def parse_lines(code: str) -> Iterator[ParsedLine]:
+    for lineno, line in enumerate(code.splitlines(), start=1):
+        yield parse_line(line, lineno)
 
 
 def main() -> None:
     args = parse_args()
-    # print(r"\begin{minted}{python}")
-    # for a in sys.argv:
-    #    print(repr(a))
-    # print(r"\end{minted}")
 
-    with open(args.file) as f:
-        lines = f.read().splitlines()
-
-    clean_lines = []
     minted_opts = args.minted_opts.split(",") if args.minted_opts else []
 
-    # Mapping snippet name to list of (lineno, token)
-    tokens: dict[str, list[tuple[int, Token]]] = collections.defaultdict(list)
+    # Mapping snippet name to list of (lineno, token, *args)
+    tokens: dict[str, _TokenList] = collections.defaultdict(list)
 
-    for lineno, line in enumerate(lines, start=1):
-        code, comment = split_line(line)
-        clean_lines.append(code)
-        if comment is None:
+    path = args.prefix / args.file
+    try:
+        code = read_code(path, output_type=OutputType(args.output_type))
+    except IOError as e:
+        raise Error(f"Failed to read file: {e}")
+
+    for parsed in parse_lines(code):
+        if not isinstance(parsed, TokenLine):
             continue
 
-        for part in comment.split(";"):
-            token_str, snippet_pat = part.strip().split(" ", 1)
+        for part in parsed.comment.split(";"):
+            token_str, snippet_pat, *token_args = shlex_split(part.strip())
             try:
                 token = Token(token_str)
             except ValueError:
                 raise Error(f"Unknown token: {token_str}")
+
+            if token == Token.INLINE_REPLACE:
+                arg_count = 2
+            elif token == Token.HL_INLINE:
+                arg_count = 1
+            else:
+                arg_count = 0
+
+            if len(token_args) != arg_count:
+                raise Error(
+                    f"Expected {arg_count} arguments for {token}, "
+                    f"but got {len(token_args)}: {token_args} (line {parsed.lineno})"
+                )
+
             for snippet in expand_snippet_name(snippet_pat):
-                tokens[snippet].append((lineno, token))
+                tokens[snippet].append((parsed.lineno, token, token_args))
 
     # print(dict(tokens))
 
     if args.snippet:  # empty arg: full file
         minted_opts += list(tokens_to_minted_opts(tokens[args.snippet], args.snippet))
 
-    if args.show_name:
-        name = args.file.relative_to(pathlib.PurePath("code"))
-        print(r"\filenameheader{%s}" % name)
+    if args.show_name == "true":
+        print(r"\filenameheader{%s}" % args.file)
+    elif args.show_name == "phantom":
+        print(r"\vphantom{\filenameheader{%s}}" % args.file)
+
+    minted_code = "\n".join(transform_code(code.splitlines(), tokens[args.snippet]))
 
     minted_opts_str = ",".join(minted_opts)
     print(r"\begin{minted}[%s]{%s}" % (minted_opts_str, args.minted_lang))
-    print("\n".join(clean_lines))
-    print(r"\end{minted}")
+    print(minted_code)
+    print(r"\ end{minted}".replace(" ", ""))  # hack so LaTeX can read this file as example...
 
 
 if __name__ == "__main__":
--- package_code.py
+++ package_code.py
@@ -1,58 +1,300 @@
-"""Example script to get code files for distribution.
-
-This creates a .zip file from all files in code/ in the development repository
-(i.e. where you'd also have your .tex files), but with all comments stripped
-off.
-"""
-
-import argparse
+import sys
+import enum
 import pathlib
 import subprocess
+import dataclasses
 import zipfile
+import shutil
+from typing import Annotated
+
+import rich
 
 import minted_extract
+import typer
+
+REPO_ROOT = pathlib.Path(__file__).resolve().parent.parent
 
 
-def parse_args() -> argparse.Namespace:
-    parser = argparse.ArgumentParser(description="Package code for participants")
-    parser.add_argument("filename", help="Output filename", type=pathlib.Path)
-    args = parser.parse_args()
-    if args.filename.suffix != ".zip":
-        parser.error("Output filename must have .zip extension")
-    return args
+class Error(Exception):
+    pass
 
 
-def get_code_files() -> list[pathlib.Path]:
+@dataclasses.dataclass
+class ProcessedFile:
+    filename: pathlib.Path
+    contents: str
+
+
+class TrainingType(enum.Enum):
+    LONG = "long"
+    SHORT = "short"
+
+
+EXCLUDE = [
+    pathlib.Path("code", "requirements-dev.txt"),
+]
+SHORT_EXCLUDE = [
+    # FIXME can we exclude more?
+    pathlib.Path("code", "mocking"),
+]
+
+
+def get_code_files(use_git: bool = True) -> list[pathlib.Path]:
+    if not use_git:
+        code_path = REPO_ROOT / "code"
+        assert code_path.is_dir(), code_path
+        return [f for f in code_path.rglob("*") if f.is_file()]
+
+    assert (REPO_ROOT / ".git").exists(), REPO_ROOT
     proc = subprocess.run(
-        ["git", "ls-files", "code"], capture_output=True, text=True, check=True
+        ["git", "ls-files", "code"],
+        capture_output=True,
+        text=True,
+        check=True,
+        cwd=REPO_ROOT,
     )
     assert not proc.stderr, proc.stderr
     return [pathlib.Path(p) for p in proc.stdout.splitlines()]
 
 
-def process_file(path: pathlib.Path) -> str:
+def filter_files(
+    files: list[pathlib.Path],
+    type: TrainingType,
+    exclude: list[pathlib.Path],
+    tox: bool,
+    yaml: bool,
+) -> list[pathlib.Path]:
+    for f in files:
+        assert (REPO_ROOT / f).is_file(), f
+
+    exclude.extend(EXCLUDE)
+    if type == TrainingType.SHORT:
+        exclude.extend(SHORT_EXCLUDE)
+
+    if not tox:
+        exclude.append(pathlib.Path("code", "tox"))
+    if not yaml:
+        exclude.append(pathlib.Path("code", "hooks", "yaml"))
+
+    for e in exclude:
+        assert (REPO_ROOT / e).exists(), e
+
+    return [f for f in files if not any(f.is_relative_to(e) for e in exclude)]
+
+
+def process_line(line: str, lineno: int, solution: bool) -> str:
+    parsed = minted_extract.parse_line(line.rstrip(), lineno)
+    if isinstance(parsed, minted_extract.ExerciseMarkerLine) and solution:
+        return parsed.to_solution()
+    return parsed.code
+
+
+def process_file(
+    file: pathlib.Path, output_type: minted_extract.OutputType
+) -> ProcessedFile | None:
+    solution = output_type == minted_extract.OutputType.SOLUTION
     cleaned_lines = []
-    with path.open("r") as src:
-        for line in src:
-            cleaned, _comment = minted_extract.split_line(line.rstrip())
-            cleaned_lines.append(cleaned)
+    has_solution = False
 
-    return "\n".join(cleaned_lines)
+    code = minted_extract.read_code(REPO_ROOT / file, output_type=output_type)
+    for parsed in minted_extract.parse_lines(code):
+        if isinstance(parsed, minted_extract.ExerciseMarkerLine) and solution:
+            cleaned_lines.append(parsed.to_solution())
+            has_solution = True
+        else:
+            cleaned_lines.append(parsed.code)
+
+    if solution and not has_solution:
+        # skip files without solution markers
+        # FIXME what about e.g. pytest.ini?
+        return None
+
+    contents = "\n".join(cleaned_lines) + "\n"
+    return ProcessedFile(filename=file, contents=contents)
 
 
-def make_zip(files: dict[pathlib.Path, str], out_path: pathlib.Path) -> None:
-    with zipfile.ZipFile(out_path, "w") as zipf:
-        for file_path, contents in files.items():
-            print(file_path)
-            zipf.writestr(str(file_path), contents)
+def make_zip(
+    files: list[ProcessedFile],
+    filename: pathlib.Path,
+    output_type: minted_extract.OutputType,
+) -> None:
+    root_subdir = output_type.value
+    with zipfile.ZipFile(filename, "w") as zipf:
+        for file in files:
+            zip_path = root_subdir / file.filename.relative_to("code")
+            zipname = str(zip_path)
+            rich.print(zipname)
+            zipf.writestr(zipname, file.contents)
 
 
-def main() -> None:
-    args = parse_args()
-    code_files = get_code_files()
-    processed = {file: process_file(file) for file in code_files}
-    make_zip(processed, args.filename)
+def output_to_dir(
+    files: list[ProcessedFile],
+    output_path: pathlib.Path,
+    quiet: bool = False,
+    demo: bool = False,
+) -> None:
+    for file in files:
+        if demo:  # We want a code/ subdir as we have other stuff too
+            rel_path = file.filename
+        else:
+            rel_path = file.filename.relative_to("code")
+        if not quiet:
+            rich.print(rel_path)
+        file_path = output_path / rel_path
+        file_path.parent.mkdir(parents=True, exist_ok=True)
+        file_path.write_text(file.contents)
+
+
+def dir_has_markers(path: pathlib.Path, markers: set[str]) -> bool:
+    return path.is_dir() and all((path / marker).exists() for marker in markers)
+
+
+def prepare_output_dir(output_path: pathlib.Path, *, force: bool) -> None:
+    assert not output_path.suffix, output_path
+    if output_path.exists():
+        if force:
+            if dir_has_markers(
+                output_path, {"pytest.ini", "rpncalc"}
+            ) or dir_has_markers(
+                output_path, {".git", "code", "dayX.ipynb", "start_demos.sh"}
+            ):
+                shutil.rmtree(output_path)
+            else:
+                raise Error(f"Refusing to delete unknown {output_path}")
+        else:
+            raise Error(f"Output directory {output_path} already exists")
+
+
+def create_git_repo(output_path: pathlib.Path) -> None:
+    git_proc = subprocess.run(
+        ["git", "rev-parse", "--short", "HEAD"],
+        capture_output=True,
+        text=True,
+        check=True,
+        cwd=REPO_ROOT,
+    )
+    assert not git_proc.stderr, git_proc.stderr
+    commit_hash = git_proc.stdout.strip()
+
+    subprocess.run(["git", "init", "-q"], check=True, cwd=output_path)
+    subprocess.run(["git", "add", "."], check=True, cwd=output_path)
+    subprocess.run(
+        ["git", "commit", "-qm", f"Initialize code based on {commit_hash}"],
+        check=True,
+        cwd=output_path,
+    )
+    rich.print(f"Created git repository in {output_path}")
+
+
+def copy_demo_files(output_path: pathlib.Path) -> None:
+    demo_files = [
+        REPO_ROOT / "scripts" / "start_demos.sh",
+        REPO_ROOT / "other" / "dayX.ipynb",
+    ]
+    for path in demo_files:
+        shutil.copy(path, output_path)
+    rich.print("Copied demo files to output directory")
+
+
+def create_venv(output_path: pathlib.Path) -> None:
+    gitignore_file = output_path / ".gitignore"
+    gitignore_file.write_text(".venv/")  # FIXME more?
+
+    subprocess.run(["uv", "venv"], check=True, cwd=output_path)
+    subprocess.run(
+        [
+            "uv",
+            "pip",
+            "install",
+            "-r",
+            REPO_ROOT / "code" / "requirements.txt",
+            "-r",
+            REPO_ROOT / "code" / "requirements-dev.txt",
+        ],
+        check=True,
+        cwd=output_path,
+    )
+
+
+def process_all(
+    code_files: list[pathlib.Path], output_type: minted_extract.OutputType
+) -> list[ProcessedFile]:
+    return [
+        processed
+        for file in code_files
+        if (processed := process_file(file, output_type=output_type)) is not None
+    ]
+
+
+def get_output_type(solutions: bool, selftest: bool) -> minted_extract.OutputType:
+    if solutions:
+        return minted_extract.OutputType.SOLUTION
+    elif selftest:
+        return minted_extract.OutputType.SELFTEST
+    else:
+        return minted_extract.OutputType.CODE
+
+
+def main(
+    type: Annotated[TrainingType, typer.Argument(help="Type of training")],
+    output: Annotated[pathlib.Path, typer.Argument(help="Output dir/.zip file")],
+    no_git: Annotated[
+        bool, typer.Option("--no-git", help="Get files in code/ without git")
+    ] = False,
+    demo: Annotated[
+        bool,
+        typer.Option(
+            "--demo", help="Create directory with additional files for live demos"
+        ),
+    ] = False,
+    solutions: Annotated[
+        bool, typer.Option("-s", "--solutions", help="Package solutions")
+    ] = False,
+    selftest: Annotated[
+        bool, typer.Option("-t", "--selftest", help="Package selftests")
+    ] = False,
+    tox: Annotated[bool, typer.Option("--tox", help="Include tox exercises")] = False,
+    yaml: Annotated[
+        bool, typer.Option("--yaml", help="Include YAML exercises")
+    ] = False,
+    exclude: Annotated[
+        list[pathlib.Path] | None, typer.Option("-e", "--exclude", help="Exclude files")
+    ] = None,
+    force: Annotated[
+        bool, typer.Option("-f", "--force", help="Overwrite output directory")
+    ] = False,
+) -> None:
+    if output.suffix and output.suffix != ".zip":
+        raise Error("Output filename must be directory or have .zip extension")
+
+    output_type = get_output_type(solutions=solutions, selftest=selftest)
+
+    code_files = filter_files(
+        get_code_files(use_git=not no_git),
+        type=type,
+        exclude=exclude or [],
+        tox=tox,
+        yaml=yaml,
+    )
+    processed = process_all(code_files, output_type=output_type)
+    if output.suffix == ".zip":
+        if demo:
+            raise Error("Can't use --demo with zip output")
+        make_zip(processed, output, output_type=output_type)
+    else:
+        prepare_output_dir(output, force=force)
+        output_to_dir(processed, output, demo=demo)
+        if demo:
+            if no_git:
+                raise Error("Cannot create git repo without using git")
+            create_git_repo(output)
+            copy_demo_files(output)
+            create_venv(output)
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        typer.run(main)
+    except Error as e:
+        rich.print(f"[red]Error:[/] {e}")
+        sys.exit(1)
